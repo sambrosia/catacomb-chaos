962f34205358537f2de062e80f671d619b8bd23b
sambrosia <samuelcwoodruff@gmail.com>
2017-02-26 13:30:04 -0700

    Moved common particle effects for enemies to their own file

---
+++ src/enemy-effects.js
@@ -0,0 +1,51 @@
+import * as fae from "fae";
+import { app } from "./app";
+
+const particleGraphics = new PIXI.Graphics()
+.beginFill(0x601ac9)
+.drawRect(0,0,2,2)
+.endFill();
+
+const particleTexture = app.renderer.generateTexture(particleGraphics);
+
+export const poofTemplate = {
+    components: ["emitter", "timeout"],
+    parent: app.stage.characters,
+
+    ready() {
+        this.emitOptions = {
+            texture: particleTexture,
+            lifetime: 300,
+            period: 20,
+            area: 20,
+            scale: 3,
+            scaleRandom: 2,
+            rotationRandom: 1,
+            endAlpha: 0.01,
+            velocityRandom: new fae.Vector(0.5, 0.5)
+        };
+
+        this.timeout(100, "kill");
+    },
+
+    kill() {
+        this.queueDestroy();
+    }
+};
+
+export const sparkTemplate = {
+    components: ["emitter"],
+
+    ready() {
+        this.emitOptions = {
+            texture: particleTexture,
+            lifetime: 300,
+            period: 50,
+            area: 4,
+            scale: 0.5,
+            scaleRandom: 0.3,
+            endAlpha: 0.01,
+            velocityRandom: new fae.Vector(1, 1)
+        };
+    }
+};
--- src/skeleton.js
+++ src/skeleton.js
@@ -1,6 +1,8 @@
 import * as fae from "fae";
 import { app } from "./app";
 
+import { sparkTemplate, poofTemplate } from "./enemy-effects";
+
 const skeletonTemplate = {
     components: ["animatedsprite", "motion", "steering", "collision"],
     parent: app.stage.characters,
@@ -60,55 +62,6 @@ const skeletonTemplate = {
         app.player.score += 10;
 
         this.queueDestroy();
-    }
-};
-
-const particleGraphics = new PIXI.Graphics()
-.beginFill(0x601ac9)
-.drawRect(0,0,2,2)
-.endFill();
-
-const particleTexture = app.renderer.generateTexture(particleGraphics);
-
-const poofTemplate = {
-    components: ["emitter", "timeout"],
-    parent: app.stage.characters,
-
-    ready() {
-        this.emitOptions = {
-            texture: particleTexture,
-            lifetime: 300,
-            period: 20,
-            area: 20,
-            scale: 3,
-            scaleRandom: 2,
-            rotationRandom: 1,
-            endAlpha: 0.01,
-            velocityRandom: new fae.Vector(0.5, 0.5)
-        };
-
-        this.timeout(100, "kill");
-    },
-
-    kill() {
-        this.queueDestroy();
-    }
-};
-
-const sparkTemplate = {
-    components: ["emitter"],
-
-    ready() {
-        this.emitOptions = {
-            texture: particleTexture,
-            lifetime: 300,
-            period: 50,
-            area: 4,
-            scale: 0.5,
-            scaleRandom: 0.3,
-            endAlpha: 0.01,
-            velocityRandom: new fae.Vector(1, 1)
-        };
     }
 };
 

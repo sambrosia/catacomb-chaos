6710ad2c25e80aaa76f5ca9728fa27cbfecfe7a1
sambrosia <samuelcwoodruff@gmail.com>
2017-03-24 20:23:43 -0600

    Added nice sound when incrementing gold

--- src/purse.js
+++ src/purse.js
@@ -1,20 +1,34 @@
+import * as fae from "fae";
+import { app } from "./app";
+
 export default class Purse {
     constructor(app) {
         this.app = app;
-
         this.gold = Number(window.localStorage.getItem("catacombChaosGold") || 0);
+
+        // TODO: Don't forget to remove this debug thing
+        app.input.on("keydown", (key) => {
+            if (key == " ") {
+                this.gold = 0;
+            }
+        });
     }
 
     addGold(n) {
         if (n <= 0) return;
-        
-        // TODO: Play nice coin sound
-        this.gold += n;
 
-        console.log("gold:", this.gold, "(+" + n + ")");
+        const playSoundAndBumpGold = (speed) => {
+            app.resources.soundCoin.sound.play({ speed: speed });
+            this.gold++;
+        };
+
+        for (let i = 0; i < n; i ++) {
+            const speed = 0.8 + i * 0.3 / n;
+            app.timeout(i * 100 + Math.random() * 20, playSoundAndBumpGold, speed);
+        }
     }
 
-    createGoldEffect(pos) {
+    createGoldEffect(n, pos) {
 
     }
 }
